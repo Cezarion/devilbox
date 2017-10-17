c2d23ec2801fefeae359b99bb236a96c2431859c
Cezarion <cezarion@cezarion.net>
2017-10-17 23:44:33 +0200

    Move devilbox settings from www-docker to $HOME

--- install.sh
+++ install.sh
@@ -21,6 +21,6 @@ ORI_DIR='./data/www';
 echo "${BLUE} Write local environnement into ${DIR}/../www-docker/.devilbox ${NORMAL}";
 
 
-echo "# This file is generated automatically, does not edit or does not restart the install.sh script in Devilbox" > "${WEB_PATH}/.devilbox"
-echo "HOST_PATH_HTTPD_DATADIR=${WEB_PATH}" >> "${WEB_PATH}/.devilbox"
-echo "DEVILBOX_LOCAL_PATH=${SRC}" >> "${WEB_PATH}/.devilbox"
+echo "# This file is generated automatically, does not edit or does not restart the install.sh script in Devilbox" > "${HOME}/.devilbox"
+echo "HOST_PATH_HTTPD_DATADIR=${WEB_PATH}" >> "${HOME}/.devilbox"
+echo "DEVILBOX_LOCAL_PATH=${SRC}" >> "${HOME}/.devilbox"
